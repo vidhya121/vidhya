4489047f006b4ef2b79132bd13cdb481c833d8aa
vidhya121 <gaganashreem23@gmail.com>
2026-07-15 14:53:32 +0530

    Add files via upload

---
+++ vidya3.py
@@ -0,0 +1,9 @@
+x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+z=int(input("enter the third number"))
+if(x>y):
+  print("x is largest number")
+if(y>z):
+  print("y is largest number")
+else:
+  print("c is largest number")
