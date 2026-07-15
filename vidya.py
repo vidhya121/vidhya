aba2cc58882e80c06036ed44c032318bfbda00b8
vidhya121 <gaganashreem23@gmail.com>
2026-07-15 14:55:55 +0530

    Add files via upload

---
+++ vidya.py
@@ -0,0 +1,10 @@
+num1=float(input("enter the first number"))
+num2=float(input("enter the second number"))
+sum=num1+num2
+dif=num1-num2
+mult=num1*num2
+div=num1/num2
+print(sum)
+print(dif)
+print(mult)
+print(div)
